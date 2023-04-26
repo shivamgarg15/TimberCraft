06c862fe43b8b8920d63587164bde00fef00ca67
Shivam Garg <shivamgarg2625@gmail.com>
2023-04-26 13:40:48 +0530

    initial

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">TimberCraft</a>
+    <a className="navbar-brand" href="/">Timber</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>

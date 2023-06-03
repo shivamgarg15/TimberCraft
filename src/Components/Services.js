17f4d8cc0dd73dbf6a2b73c338309b2a162a45f0
Shivam Garg <shivamgarg2625@gmail.com>
2023-06-03 21:38:35 +0530

    as

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -37,6 +37,28 @@ function Services() {
                     </div>
                 </div>
             </div>
+            <div className='py-4'>
+                <h1 className='text-center'>Elevation</h1>
+                <div className="m-auto" style={{ width: "90%" }}>
+                    <div className="row gx-4 gy-4">
+                        {img.interior.map((element,index) => {
+                            return (<div className="col-12 col-sm-6 col-lg-4 flex-grow-1" key={index} ><img className='servicesImg' src={element} alt="" height={"100%"} width={"100%"} style={{maxHeight: "400px", objectFit: "cover"}}/></div>);
+                        })
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className='py-4'>
+                <h1 className='text-center'>Work Space</h1>
+                <div className="m-auto" style={{ width: "90%" }}>
+                    <div className="row gx-4 gy-4">
+                        {img.interior.map((element,index) => {
+                            return (<div className="col-12 col-sm-6 col-lg-4 flex-grow-1" key={index} ><img className='servicesImg' src={element} alt="" height={"100%"} width={"100%"} style={{maxHeight: "400px", objectFit: "cover"}}/></div>);
+                        })
+                        }
+                    </div>
+                </div>
+            </div>
 
         </>
     )

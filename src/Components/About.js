b0625aa1eefaea73504c4d99e119080c922b186d
Shivam Garg <shivamgarg2625@gmail.com>
2023-06-11 11:27:42 +0530

    about rephare and images

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade';
-// import design from '../Images/contact1.jpg'
+import design1 from '../Images/about-grid/aboutgrid-1.jpg'
+import design2 from '../Images/about-grid/aboutgrid-2.jpg'
+import design3 from '../Images/about-grid/aboutgrid-3.jpg'
+import design4 from '../Images/about-grid/aboutgrid-4.jpg'
+import design5 from '../Images/about-grid/aboutgrid-5.jpg'
+import design6 from '../Images/about-grid/aboutgrid-6.jpg'
+
+
 
 function About() {
   return (
@@ -11,7 +18,7 @@ function About() {
           <div className='container boxShadow body1Column' style={{ width: "90%" }}>
             <h1 className='text-center'>About Us</h1>
             <p>
-              Welcome to our store TimberCraft! We are passionate about helping people create beautiful and functional homes with our wide range of furniture and interior designs.
+              Welcome to our store <b>TimberCraft!</b> We are passionate about helping people create beautiful and functional homes with our wide range of furniture and interior designs.
 
               We offer a wide variety of styles and designs, from classic to modern, to fit every taste and budget.
 
@@ -46,37 +53,41 @@ function About() {
 
         <div className="container text-center height-inherit primary-font" style={{ width: "90%" }}>
           <div className="row justify-content-center height-inherit py-4">
-            {/* <img src="/pix/samples/23m.jpg" alt="Sample photo"> */}
             <Fade left delay={100}>
               <div className="col-10 col-md-5 col-xl-3 boxShadow my-3 mx-4 p-3" style={{ minHeight: "400px" }}>
                 <h3 style={{ textDecoration: "underline" }}>DESIGN PHILOSOPHY</h3>
-                {/* <img src={design} alt="" /> */}
-                <p>With our design philosophy as our guiding principle, we aim to bring timeless elegance, comfort, and joy to your living spaces.</p>
+                <img src={design6} alt="" />
+                <p><b>TimberCraft</b> enables us to bring the design of your dreams to life!</p>
                 </div>
 
               <div className="col-10 col-md-5 col-xl-3 boxShadow my-3 mx-4 p-3" style={{ minHeight: "400px" }}>
                 <h3 style={{ textDecoration: "underline" }}>QUALITY AND CRAFTSMANSHIP</h3>
-                <p>We believe that furniture should not only be visually stunning but also built to withstand the test of time.</p>
+                <img src={design3} alt="" />
+                <p>We construct with the same care and attention as if it were our own home. </p>
               </div>
 
               <div className="col-10 col-md-5 col-xl-3 boxShadow my-3 mx-4 p-3" style={{ minHeight: "400px" }}>
                 <h3 style={{ textDecoration: "underline" }}>EXTERIOR</h3>
-                <p>Transform your space into an enchanting oasis with our exquisite collection of finely crafted furniture.</p>
+                <img src={design5} alt="" />
+                <p>Create an enchanting oasis with our exquisite furniture collection, transforming your space beautifully.</p>
                </div>
 
               <div className="col-10 col-md-5 col-xl-3 boxShadow my-3 mx-4 p-3" style={{ minHeight: "400px" }}>
                 <h3 style={{ textDecoration: "underline" }}>DECORATION</h3>
-                <p>Elevate your living space with our unparalleled selection of stylish and curated decor.</p>
+                <img src={design4} alt="" />
+                <p>Enhance your space with our unmatched, stylish decor selection.</p>
                 </div>
 
               <div className="col-10 col-md-5 col-xl-3 boxShadow my-3 mx-4 p-3" style={{ minHeight: "400px" }}>
                 <h3 style={{ textDecoration: "underline" }}>INTERIOR PLANNING</h3>
-                <p>Experience the power of interior planning and discover a world of possibilities for your living spaces with our exceptional furniture.</p>
+                <img src={design2} alt="" />
+                <p>Unleash the potential of interior planning and exceptional furniture with <b>TimberCraft!</b></p>
                  </div>
 
               <div className="col-10 col-md-5 col-xl-3 boxShadow my-3 mx-4 p-3" style={{ minHeight: "400px" }}>
                 <h3 style={{ textDecoration: "underline" }}>STYLE SELECTION</h3>
-                <p>Allowing you to create a truly unique and harmonious ambiance in your home with our exceptional furniture selection</p>
+                <img src={design1} alt="" />
+                <p>Craft a distinct and harmonious atmosphere in your home with our exceptional furniture choices.</p>
                 </div>
             </Fade>
           </div>
